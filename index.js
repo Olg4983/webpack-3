bd5fb41055b64d83323603f20aeae4d3e1f667e9
Olga Shmakova <olg4983@gmail.com>
2021-11-13 21:27:55 +0200

    Webpack 18.4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import "./index.css";
+import JS_IMG from './assets/js.jpg';
 console.log('Hello World!');
+
+const jsImgHTML = document.createElement('img');
+jsImgHTML.className = 'js-img';
+jsImgHTML.src = JS_IMG;
+
+document.body.append(jsImgHTML);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { template } = require('lodash');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
@@ -7,4 +11,44 @@ module.exports = {
         filename: 'build.js',
         path: path.resolve(__dirname, 'build'),
     },
+    devServer: {
+        //contentBase: './build',
+        open: true,
+        compress: true,
+        port: 4000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    }
 };
